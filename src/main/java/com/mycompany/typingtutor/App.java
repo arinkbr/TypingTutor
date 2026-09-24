f6552aef6ec93c1bf3385a2d3421b1141aeca4d6
arin.kbr <arin.bornil@gmail.com>
2026-09-23 23:52:04 -0400

    Add practice sentence and typing response field

--- src/main/java/com/mycompany/typingtutor/App.java
+++ src/main/java/com/mycompany/typingtutor/App.java
@@ -1,30 +1,56 @@
 package com.mycompany.typingtutor;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
-
 /**
- * JavaFX App
+ * A program for practice typing.
+ *
+ * @author Arin Kabir
  */
 public class App extends Application {
 
     @Override
     public void start(Stage stage) {
-        var javaVersion = SystemInfo.javaVersion();
-        var javafxVersion = SystemInfo.javafxVersion();
+        Label titleLabel = new Label("Typing Tutor");
 
-        var label = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-        var scene = new Scene(new StackPane(label), 640, 480);
+        Label instructionLabel = new Label(
+                "Type the following sentence:");
+
+        TextField targetField = new TextField(
+                "Try typing this text. Do it as quickly and accurately as you can.");
+        targetField.setEditable(false);
+
+        Label responseLabel = new Label("Your response:");
+
+        TextField responseField = new TextField();
+        responseField.setPromptText("Start typing here");
+
+        VBox root = new VBox(15);
+        root.setPadding(new Insets(20));
+        root.getChildren().addAll(
+                titleLabel,
+                instructionLabel,
+                targetField,
+                responseLabel,
+                responseField
+        );
+
+        Scene scene = new Scene(root, 800, 300);
+
+        stage.setTitle("Typing Tutor");
         stage.setScene(scene);
         stage.show();
+
+        responseField.requestFocus();
     }
 
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
-
 }
